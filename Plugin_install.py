3bceaabe824d9a2f026a965111508db71d4ef5ad
justasth <120112483+justasth@users.noreply.github.com>
2025-06-11 13:39:56 +0800

    Update Plugin_install.py

--- Plugin_install.py
+++ Plugin_install.py
@@ -1,3 +1,7 @@
+插件介绍：
+https://zhuanlan.zhihu.com/p/395827350
+https://www.bilibili.com/opus/358433334599617836
+下载网址：https://www.food4rhino.com/en
 .gha
 File-Special Folders-Components Folder
 .ghuser
